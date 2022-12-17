0c60864433fbe98e8b74fed5f88ccbfac0f2a326
JDstibling <stiblingjd@gmail.com>
2022-12-17 16:36:03 +0100

    fix game Function

--- kaa/src/app/quiz/quiz.component.ts
+++ kaa/src/app/quiz/quiz.component.ts
@@ -18,17 +18,13 @@ export class QuizComponent implements OnInit {
   //pictureInterface = PictureInterface;
   casting: any = [];
   citation: ({ citation: string; infos: { acteur: string; personnage: string; auteur: string; saison: string; episode: string; }; id: number; } | { citation: string; infos: { auteur: string; acteur: string; personnage: null; saison: string; episode: string; }; id: number; } | { citation: string; infos: { auteur: string; acteur: string; personnage: string; saison: string; episode?: undefined; }; id: number; })[] = [];
-  randomIndexNum0 = 0;
-  randomIndexNum1 = 0;
-  randomIndexNum2 = 0;
-  randomIndexNum3 = 0;
   randomIndex : number[]= [];
   randomNumber: number[]= [];
   event: any;
   stepQuiz = 0;
   endStatus= "none";
-  orderIndex!: number;
   currentItem: number | undefined;
+  idpersoAtRandomCitation!: number;
 
   constructor(private CitationService: CitationService) { }
 
@@ -45,73 +41,72 @@ export class QuizComponent implements OnInit {
     
     //boucle sur toutes les questions du quiz
     for (let itemQuiz = 0; itemQuiz < 10; itemQuiz++) {
-      
-      // attribution de chiffres aléatoires
-      this.randomNumber = [];
-      for (let i = -1; i < 3; i++) {
-        // pour mélanger l'ordre des images mauvaise avec la bonne
-        this.randomIndex.push(Math.floor(Math.random() * 4 -2));
-        
-        // pour aller chercher aléatoirement des personnages pour les fausses réponses
-        this.randomNumber.push(Math.floor(Math.random() * this.casting.length));
-      }
-      //console.log(this.randomIndex );
-      this.orderIndex = itemQuiz;
-      //console.log(this.orderIndex);
-      
-
-  
+    
       // récupération d'une citation aléatoire (bonne réponse)
-       const randomCitation = this.citation[Math.floor(Math.random() * this.citation.length)];
-       //console.log(randomCitation);
-
+      const randomCitation = this.citation[Math.floor(Math.random() * this.citation.length)];
+      //console.log(randomCitation);
+      
       //attribution de la citation pour chaque questions (10 au total)
       this.quiz[itemQuiz].citation = randomCitation.citation;
       
       // récupération du nom du personnage associé (bonne réponse)
       const persoAtRandomCitation = randomCitation.infos.personnage;
-  
+      
       // récupération de l'image du personnage concerné (bonne réponse)
       this.pictureAtRandomCitation = '../../assets/images/persos/' + persoAtRandomCitation + '.jpg';
       this.quiz[itemQuiz].picture.goodPicture = this.pictureAtRandomCitation;
-  
+      
+      //recherche de l'id du personnage de la bonne réponde dans la liste de casting pour ne pas qu'il soit sélectionné parmis les maubvaise réponses
+      this.casting.forEach((element: {
+        id: any; personnage: any; 
+      }) => {
+        if (element.personnage === persoAtRandomCitation){
+          // récupération de l'id du personnage de la sitation sélectionné
+          this.idpersoAtRandomCitation = element.id;
+        }
+      });
+      
+      // attribution de chiffres aléatoires
+      this.randomNumber = [];
+      for (let i = 0; i < 2; i++) {
+
+        // pour mélanger l'ordre des images mauvaise avec la bonne
+        this.randomIndex.push(Math.floor(Math.random() * 6 -2));
+      
+        // pour aller chercher aléatoirement des personnages pour les fausses réponses
+        let addNumberRandom = Math.floor(Math.random() * this.casting.length);
+
+        if (addNumberRandom == this.idpersoAtRandomCitation){
+          addNumberRandom ++;
+          this.randomNumber.push(addNumberRandom);
+        }else {
+          this.randomNumber.push(addNumberRandom);
+        }
+      }
+      
+      
+      
       // //récupération d'une image aléatoire non égale à la premiere (première mauvaise réponse)
-       let randomPerso1 = this.casting[this.randomNumber[0]].personnage;
-       //console.log("randomPerso1 " + this.randomNumber[0]);
-       
-       let randomPerso2 = this.casting[this.randomNumber[1]].personnage;
-       //console.log("randomPerso2 " + this.randomNumber[1])
+      let randomPerso1 = this.casting[this.randomNumber[0]].personnage;
+      
+      let randomPerso2 = this.casting[this.randomNumber[1]].personnage;
 
       // vérification si pas de doublon pour éviter d'avoir une mauvaise réponse identique à la bonne
-      //console.log("perso ok: " + persoAtRandomCitation + "les autres : " + randomPerso1 + randomPerso2);
-      //console.log(this.randomNumber);
-      
-      
-       if((randomPerso1 === persoAtRandomCitation) || (randomPerso1 === randomPerso2)) {
-         this.randomNumber[0] +=1;
-         this.randomNumber[1] +=3;
-         this.randomNumber[2] +=2;
-         //console.log(this.randomNumber);
+
+       if(this.randomNumber[0] == this.randomNumber[1]) {
+        this.randomNumber[0] += 1;
          this.quiz[itemQuiz].picture.noGoodPicture1 = '../../assets/images/persos/' + randomPerso1 + '.jpg';
+         this.quiz[itemQuiz].picture.noGoodPicture2 = '../../assets/images/persos/' + randomPerso2 + '.jpg';
         } else {
           this.quiz[itemQuiz].picture.noGoodPicture1 = '../../assets/images/persos/' + randomPerso1 + '.jpg';
-        }
-      if(randomPerso2 === persoAtRandomCitation) {
-        this.randomNumber[1] +=2;
-        this.randomNumber[2] +=1;
-        //console.log(this.randomNumber);
-        this.quiz[itemQuiz].picture.noGoodPicture2 = '../../assets/images/persos/' + randomPerso2 + '.jpg';
-      } else {
           this.quiz[itemQuiz].picture.noGoodPicture2 = '../../assets/images/persos/' + randomPerso2 + '.jpg';
         }
-      //return quiz
-      
     }
-  //console.log(this.quiz);
   }
 
   restartGame(){
     this.quizGenerate();
+    this.stepQuiz = 0;
     if(this.stepQuiz === 0 && this.endStatus === "none"){
       this.endStatus = "initial";
     }
