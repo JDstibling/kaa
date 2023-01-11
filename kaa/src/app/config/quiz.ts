31e3f2f319af5246638f234a51988e5357d4e21d
JDstibling <stiblingjd@gmail.com>
2023-01-11 10:55:34 +0100

    timing animation quiz

--- kaa/src/app/config/quiz.ts
+++ kaa/src/app/config/quiz.ts
@@ -1,60 +1,70 @@
 export const quiz: Array<quizInterface> = [
     {
         citation: '',
+        persoAtRandomCitation: '',
         picture: [],
         result: false,
         id :1
     },
     {
         citation: '',
+        persoAtRandomCitation: '',
         picture: [],
         result: false,
         id :2
     },
     {
         citation: '',
+        persoAtRandomCitation: '',
         picture: [],
         result: false,
         id :3
     },
     {
         citation: '',
+        persoAtRandomCitation: '',
         picture: [],
         result: false,
         id :4
     },
     {
         citation: '',
+        persoAtRandomCitation: '',
         picture: [],
         result: false,
         id :5
     },
     {
         citation: '',
+        persoAtRandomCitation: '',
         picture: [],
         result: false,
         id :6
     },
     {
         citation: '',
+        persoAtRandomCitation: '',
         picture: [],
         result: false,
         id :7
     },
     {
         citation: '',
+        persoAtRandomCitation: '',
         picture: [],
         result: false,
         id :8
     },
     {
         citation: '',
+        persoAtRandomCitation: '',
         picture: [],
         result: false,
         id :9
     },
     {
         citation: '',
+        persoAtRandomCitation: '',
         picture: [],
         result: false,
         id :10
@@ -66,9 +76,10 @@ export const quiz: Array<quizInterface> = [
 
 export interface quizInterface {
     citation: string;
+    persoAtRandomCitation: string | null;
     picture: Array<pictureInterface> | any;
     result: boolean;
-    id: number
+    id: number;
 }
 
 export interface pictureInterface {
--- kaa/src/app/quiz/quiz.component.ts
+++ kaa/src/app/quiz/quiz.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { animate, state, style, transition, trigger } from '@angular/animations';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 import { quiz } from '../config/quiz';
 
@@ -7,7 +8,29 @@ import { CitationService } from '../services/citation.service';
 @Component({
   selector: 'app-quiz',
   templateUrl: './quiz.component.html',
-  styleUrls: ['./quiz.component.scss']
+  styleUrls: ['./quiz.component.scss'],
+  animations: [
+    trigger('openClose', [
+      state(
+        'end',
+        style({
+          opacity: 0,
+          transform: 'translateX(10px)',
+        })
+      ),
+      state(
+        'start',
+        style({
+          opacity: 1,
+          transform: 'translateX(0px)',
+        })
+      ),
+      transition('start => end', animate('.25s')),
+      transition('end => start', animate('.25s')),
+    ]),
+  ],
+  encapsulation: ViewEncapsulation.None,
+  
 })
 export class QuizComponent implements OnInit {
 
@@ -15,26 +38,32 @@ export class QuizComponent implements OnInit {
   pictureAtRandomCitation: string = "";
   quiz = quiz;
   quizPicture = [];
-  //pictureInterface = PictureInterface;
   casting: any = [];
   citation: ({ citation: string; infos: { acteur: string; personnage: string; auteur: string; saison: string; episode: string; }; id: number; } | { citation: string; infos: { auteur: string; acteur: string; personnage: null; saison: string; episode: string; }; id: number; } | { citation: string; infos: { auteur: string; acteur: string; personnage: string; saison: string; episode?: undefined; }; id: number; })[] = [];
   randomIndex : number[]= [];
   randomNumber: number[]= [];
   event: any;
   stepQuiz = 0;
-  endStatus= "none";
+  endStatus= 0;
   currentItem: number | undefined;
   idpersoAtRandomCitation!: number;
+  startGame = true;
+  textButton = "Jouer";
+  resultBox!: string[];
+  randomPerso1: any;
+  randomPerso2: any;
+  winCount = 0;
+  loseCount = 0;
+  state = "end";
+  
 
   constructor(private CitationService: CitationService) { }
 
   ngOnInit(): void {
     this.citation = this.CitationService.getCitations();
     this.casting = this.CitationService.getCasting();
-    //console.log(this.citation);
     this.quizGenerate();
-    console.log(this.quiz);
-    
+    this.resultBox = [];
   }
   
   quizGenerate() {
@@ -42,32 +71,33 @@ export class QuizComponent implements OnInit {
     //boucle sur toutes les questions du quiz
     for (let itemQuiz = 0; itemQuiz < 10; itemQuiz++) {
     
+      //remmise à zéro du tableau de chiffre aléatoire pour en garder que 2
+      this.randomNumber = [];
+
       // récupération d'une citation aléatoire (bonne réponse)
       const randomCitation = this.citation[Math.floor(Math.random() * this.citation.length)];
-      //console.log(randomCitation);
       
       //attribution de la citation pour chaque questions (10 au total)
       this.quiz[itemQuiz].citation = randomCitation.citation;
       
-      // récupération du nom du personnage associé (bonne réponse)
-      const persoAtRandomCitation = randomCitation.infos.personnage;
+      //récup du nom du perso concerné par la citation (bonne réponse) :
+      this.quiz[itemQuiz].persoAtRandomCitation = randomCitation.infos.personnage;
       
       // récupération de l'image du personnage concerné (bonne réponse)
-      this.pictureAtRandomCitation = '../../assets/images/persos/' + persoAtRandomCitation + '.jpg';
+      this.pictureAtRandomCitation = '../../assets/images/persos/' + this.quiz[itemQuiz].persoAtRandomCitation + '.jpg';
       this.quiz[itemQuiz].picture.goodPicture = this.pictureAtRandomCitation;
       
       //recherche de l'id du personnage de la bonne réponde dans la liste de casting pour ne pas qu'il soit sélectionné parmis les maubvaise réponses
       this.casting.forEach((element: {
         id: any; personnage: any; 
       }) => {
-        if (element.personnage === persoAtRandomCitation){
+        if (element.personnage === this.quiz[itemQuiz].persoAtRandomCitation){
           // récupération de l'id du personnage de la sitation sélectionné
           this.idpersoAtRandomCitation = element.id;
         }
       });
       
       // attribution de chiffres aléatoires
-      this.randomNumber = [];
       for (let i = 0; i < 2; i++) {
 
         // pour mélanger l'ordre des images mauvaise avec la bonne
@@ -76,69 +106,103 @@ export class QuizComponent implements OnInit {
         // pour aller chercher aléatoirement des personnages pour les fausses réponses
         let addNumberRandom = Math.floor(Math.random() * this.casting.length);
 
+        // si numéro aléatoire égale à l'id de la bonne réponse, prendre l'id suivant
         if (addNumberRandom == this.idpersoAtRandomCitation){
           addNumberRandom ++;
           this.randomNumber.push(addNumberRandom);
         }else {
           this.randomNumber.push(addNumberRandom);
         }
+
+        console.log(this.randomNumber);
+        
+
       }
       
+      // //récupération d'une image aléatoire non égale à la premiere  (mauvaise réponse)
+      this.randomPerso1 = this.casting[this.randomNumber[0]].personnage;
       
-      
-      // //récupération d'une image aléatoire non égale à la premiere (première mauvaise réponse)
-      let randomPerso1 = this.casting[this.randomNumber[0]].personnage;
-      
-      let randomPerso2 = this.casting[this.randomNumber[1]].personnage;
+      this.randomPerso2 = this.casting[this.randomNumber[1]].personnage;
 
       // vérification si pas de doublon pour éviter d'avoir une mauvaise réponse identique à la bonne
 
        if(this.randomNumber[0] == this.randomNumber[1]) {
         this.randomNumber[0] += 1;
-         this.quiz[itemQuiz].picture.noGoodPicture1 = '../../assets/images/persos/' + randomPerso1 + '.jpg';
-         this.quiz[itemQuiz].picture.noGoodPicture2 = '../../assets/images/persos/' + randomPerso2 + '.jpg';
+         this.quiz[itemQuiz].picture.noGoodPicture1 = '../../assets/images/persos/' + this.randomPerso1 + '.jpg';
+         this.quiz[itemQuiz].picture.noGoodPicture2 = '../../assets/images/persos/' + this.randomPerso2 + '.jpg';
         } else {
-          this.quiz[itemQuiz].picture.noGoodPicture1 = '../../assets/images/persos/' + randomPerso1 + '.jpg';
-          this.quiz[itemQuiz].picture.noGoodPicture2 = '../../assets/images/persos/' + randomPerso2 + '.jpg';
+          this.quiz[itemQuiz].picture.noGoodPicture1 = '../../assets/images/persos/' + this.randomPerso1 + '.jpg';
+          this.quiz[itemQuiz].picture.noGoodPicture2 = '../../assets/images/persos/' + this.randomPerso2 + '.jpg';
         }
     }
   }
 
   restartGame(){
+    this.state = "start";
     this.quizGenerate();
+    this.resultBox = [];
     this.stepQuiz = 0;
-    if(this.stepQuiz === 0 && this.endStatus === "none"){
-      this.endStatus = "initial";
+    this.startGame = true;
+    this.winCount = 0;
+    this.loseCount = 0;
+    if(this.stepQuiz === 0 && this.endStatus === 0){
+      this.endStatus = 1;
+      this.textButton = "Relancer le quiz"
     }
   }
 
   game(event:any){
+
+    this.state = "end";
     //récupère l'id de la question en cours
     let stringToConvert = document.getElementById("count")?.innerHTML;
     let numberValue = Number(stringToConvert);
     this.currentItem = numberValue;
-  
-    //récupération de l'id de l'image choisis au click
+
     if(event.target.id === "g"){
-      //alert("gagné !!!")
+      this.winCount ++;
       this.quiz[this.currentItem -1].result = true;
+      let str = this.quiz[this.currentItem-1].picture.goodPicture
+      let subStr = str.substring(27);
+      subStr = subStr.substring(0, subStr.length - 4);
+      this.resultBox.push(subStr);
     }else {
-      //alert("perdu!!!!!");
+      this.loseCount ++;
       this.quiz[this.currentItem -1].result = false;
+      if (event.target.id === "n1"){
+        let str = this.quiz[this.currentItem-1].picture.noGoodPicture1
+        let subStr = str.substring(27);
+        subStr = subStr.substring(0, subStr.length - 4)
+        this.resultBox.push(subStr);
+        
+      }else if (event.target.id === "n2"){
+        let str = this.quiz[this.currentItem-1].picture.noGoodPicture2
+        let subStr = str.substring(27);
+        subStr = subStr.substring(0, subStr.length - 4)
+        this.resultBox.push(subStr);
+      }
+      
     }
-    this.stepQuiz +=1;
-    if(this.stepQuiz === 10){
-      this.stepQuiz = 0;
-      this.endStatus = "none";
-      let countgoodAnswer = 0;
-      this.quiz.forEach(element => {
-        if(element.result === true){
-          countgoodAnswer ++;
-        }
-      });
-      alert("vous avez un résultat de : " + countgoodAnswer)
-    }
+    setTimeout(() => {
+      this.stepQuiz +=1;
+      if(this.stepQuiz === 10){
+        this.textButton = "Lancer le quiz";
+        this.stepQuiz = 0;
+        this.endStatus = 0;
+        // compteur de bonne réponses
+        let countgoodAnswer = 0;
+        this.quiz.forEach(element => {
+          if(element.result === true){
+            countgoodAnswer ++;
+          }
+          this.startGame = false;
+        });
+        alert("vous avez un résultat de : " + countgoodAnswer)
+      };
+      console.log("Delayed for 1 second.");
+      this.state = "start";
+    }, 1000)
+
     // à la fin du jeu (prévoir ensuite une méthode end game !)
   }
-
 }
