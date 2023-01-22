8662f9272c867c329e1bdd5a8029bcb1d3ba3b24
JDstibling <stiblingjd@gmail.com>
2023-01-22 23:39:29 +0100

    install toast test

--- kaa/src/app/components/sign-in/sign-in.component.ts
+++ kaa/src/app/components/sign-in/sign-in.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { Route, Router } from '@angular/router';
+import { Router } from '@angular/router';
 import { AuthService } from "../../services/auth.service";
 
 // new version
