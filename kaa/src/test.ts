112d826b1e30387cbc7fb784c1d345ae42ef26ab
JDstibling <stiblingjd@gmail.com>
2023-01-23 11:10:58 +0100

    fix maj angular

--- kaa/src/test.ts
+++ kaa/src/test.ts
@@ -1,7 +1,7 @@
 // This file is required by karma.conf.js and loads recursively all the .spec and framework files
 
 import 'zone.js/testing';
-import { getTestBed } from '@angular/core/testing';
+
 import {
   BrowserDynamicTestingModule,
   platformBrowserDynamicTesting
@@ -14,11 +14,7 @@ declare const require: {
   };
 };
 
-// First, initialize the Angular testing environment.
-getTestBed().initTestEnvironment(
-  BrowserDynamicTestingModule,
-  platformBrowserDynamicTesting(),
-);
+
 
 // Then we find all the tests.
 const context = require.context('./', true, /\.spec\.ts$/);
