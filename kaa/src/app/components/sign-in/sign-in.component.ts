de6582b4fe1f9490d21cfde2bc6d297c16b1f095
JDstibling <stiblingjd@gmail.com>
2023-01-23 10:25:30 +0100

    install toast test fix

--- kaa/src/app/components/sign-in/sign-in.component.ts
+++ kaa/src/app/components/sign-in/sign-in.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { UntypedFormControl, UntypedFormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from "../../services/auth.service";
 
@@ -16,9 +16,9 @@ import { AuthService } from "../../services/auth.service";
 })
 export class SignInComponent implements OnInit {
 
-  loginForm = new FormGroup({
-    email: new FormControl('',[Validators.required, Validators.email]),
-    password: new FormControl('', Validators.required)
+  loginForm = new UntypedFormGroup({
+    email: new UntypedFormControl('',[Validators.required, Validators.email]),
+    password: new UntypedFormControl('', Validators.required)
   })
 
   // loginForm = this.fb.group({
